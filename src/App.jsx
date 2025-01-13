7dfd3fa41f08b232a67361a376b171f89098f507
Brian Ho <brian.ho@jucyworld.com>
2025-01-13 17:17:01 +1300

    Simple examples of mocking API with msw

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const onSendClick = (e) => {
+    e.preventDefault();
+    fetch("/api/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "token123-pass-pass"
+      },
+      body: JSON.stringify({ content }),
+    });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <input value={content} onChange={(e) => setContent(e.target.value)} />
+        <button onClick={onSendClick}>Send</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,25 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-    http.get('/api/users', () => {
+    http.get('/api/users', (resolver) => {
         return HttpResponse.json([
             { id: 1, name: 'John Maverick' },
             { id: 2, name: 'Jane Doe' },
         ])
+    }),
+    http.post('/api/messages', async ({ request }) => {
+        const authToken = request.headers.get('Authorization');
+        if (!authToken) {
+            return HttpResponse.json({ msg: "Unauthorized" }, { status: 401 });
+        }
+        const requestBody = await request.json();
+        console.log('requestBody:\n', requestBody);
+        return HttpResponse.json({
+            content: requestBody.content,
+            createdAt: new Date().toLocaleString()
+        }, {
+            status: 201
+        });
     })
 ];
 
